55cbb9bf397946a05f5498d3dfb5e86480680da4
swricci <swbrown@ncsu.edu>
2021-11-03 14:04:53 -0400

    update cv

--- to_run.R
+++ to_run.R
@@ -1,15 +1,15 @@
-datadrivencv::use_datadriven_cv(
-  full_name = "Shannon Ricci",
-  data_location = "https://docs.google.com/spreadsheets/d/1LqX7AzJDC03nr9K6mF6LdEPIoHdADBuLbi_IDqlKTnc/edit#gid=917338460",
-  pdf_location = "https://github.com/swricci/my-cv/blob/main/ricci_cv.pdf",
-  html_location = "https://swricci.github.io/my-cv/",
-  source_location = "https://github.com/swricci/my-cv")
-
+# datadrivencv::use_datadriven_cv(
+#   full_name = "Shannon Ricci",
+#   data_location = "https://docs.google.com/spreadsheets/d/1LqX7AzJDC03nr9K6mF6LdEPIoHdADBuLbi_IDqlKTnc/edit#gid=917338460",
+#   pdf_location = "https://github.com/swricci/my-cv/blob/main/ricci_cv.pdf",
+#   html_location = "https://swricci.github.io/my-cv/",
+#   source_location = "https://github.com/swricci/my-cv")
+# 
 
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "cv.html")
+                  output_file = "index.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
